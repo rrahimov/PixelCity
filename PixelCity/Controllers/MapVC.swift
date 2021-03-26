c3a5c2ed79945ad3a03d3f9acdb8afc3debe1608
Ruhullah Rahimov <ruhullah.rahimov@gmail.com>
2021-03-26 22:23:08 +0400

    1

---
+++ PixelCity/Controllers/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  PixelCity
+//
+//  Created by Ruhullah Rahimov on 26.03.21.
+//
+
+import UIKit
+
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
